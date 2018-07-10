9720059c5fb67f9690d400efed3c50f1e83b2b2b
Mark Chmarny <mark@chmarny.com>
2018-07-10 10:17:44 -0700

    Added simple app, docker

---
+++ app.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Simple app running...")
+	target := os.Getenv("SIMPLE_MSG")
+	if target == "" {
+		target = "SIMPLE_MSG variable not defined"
+	}
+	fmt.Fprintf(w, "Hello World: %s!\n", target)
+}
+
+func main() {
+	flag.Parse()
+	log.Print("Simple app server started...")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
